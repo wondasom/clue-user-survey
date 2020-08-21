0ac3d911afbf116f1b55d25c75d4f3fbb91b5a04
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-08-21 21:28:09 +0200

    create Clue color palette

--- src/constants/style.js
+++ src/constants/style.js
@@ -1,7 +1,14 @@
+// color palette of Clue
 export const COLORS = {
-	mainBlue: '#1f39ca',
-	mainAqua: '#48bea2',
+	red: '#e63e33',
+	pink: '#f3b8b8',
+	blue: '#144181',
+	orange: '#eb6133',
+	turquoise: '#46916f',
+	aqua: '#36829b',
+	green: '#8ba52c',
 	white: '#fefefe',
-	grey:'#333333',
-	lightGrey:'#727272'
+	grey: '#333333',
+	lightGrey: '#f8f4f7',
+	yellow: '#f8c433'
 };
